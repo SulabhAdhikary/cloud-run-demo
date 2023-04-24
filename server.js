16b5a0a915638197c564b307a1f5cc52062e437b
sulabh adhikary <sulabhadhikary@gmail.com>
2023-04-23 22:53:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http =  require('http');
 
 const server = http.createServer((req, res)=> {
     console.log('server created');
-    res.end("hello awsone");
+    res.end("hello awsone send now");
 
 })
 
